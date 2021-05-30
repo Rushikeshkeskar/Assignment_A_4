2c2493b85be9b99043791d208e68e2473c6dd68a
Rushikeshkeskar <81505552+Rushikeshkeskar@users.noreply.github.com>
2021-05-30 20:06:20 +0530

    Add files via upload

---
+++ Assignment 4.php
@@ -0,0 +1,19 @@
+<?xml vesrion="1.o" ?>
+
+
+<Mayanagari>
+            <Classical>
+			   <Movie_Name>Casablanca</Movie_Name>
+			   <Release_Year>1942</Release_Year>
+			   </Classical>
+
+            <Action>
+			   <Movie_Name>Mad Max Fury Road</Movie_Name>
+			   <Release_Year>2015</Release_Year>
+			   </Action>
+
+            <Horror>
+			   <Movie_Name>It</Movie_Name>
+			   <Release_Year>2017</Release_Year>
+			   </Action>
+</Mayanagari>
